80377da3768244dfc1550cca909f45dd31d650a9
mogalia786 <imac@Heros-iMac.local>
2016-09-28 14:11:40 +0200

    Added Function

--- GIT1/ViewController.swift
+++ GIT1/ViewController.swift
@@ -12,12 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func Test(AS : inout Int){
+    
+        AS=100
+        
     }
 
 
